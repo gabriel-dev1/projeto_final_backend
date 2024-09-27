428dd4451f34b832eb22a26de93285079b214fc4
Gabriel <127807744+gabriel-dev1@users.noreply.github.com>
2024-09-27 14:50:38 -0300

    Upload

--- app/models.py
+++ app/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=200, unique=True, null=False, blank=False)
+    title = models.CharField(max_length=150, unique=True, null=False, blank=False)
     content = models.TextField(max_length=200, null=False, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post')
+    author = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE, related_name='post')
 
     class Meta:
         ordering = ['-created_at']
