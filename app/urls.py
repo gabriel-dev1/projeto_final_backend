c6164d9faca02a639b10777fc925afdead6214b1
Gabriel <127807744+gabriel-dev1@users.noreply.github.com>
2024-09-30 14:52:31 -0300

    Upload

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from app.views import index, register, home, logout
+from app.views import index, register, home, logout, editar_post, deletar_post
 
 urlpatterns = [ 
     path('', index, name='index'), 
     path('register', register, name='register'),
     path('home', home, name='home'),
     path('logout', logout, name='logout'),
+    path('editar-post/<int:post_id>', editar_post, name='editar_post'),
+    path('deletar-post/<int:post_id>', deletar_post, name='deletar_post'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect
-from app.forms import Login, RegisterForm, PostForm
-from .models import Post
 from django.contrib.auth.models import User
 from django.contrib import auth
 from django.contrib import messages
+from app.forms import Login, RegisterForm, PostForm
+from .models import Post
 
 # Create your views here.
 def index(request):
@@ -64,9 +64,9 @@ def register(request):
     return render(request, 'register.html', {'form': form})
 
 def home(request):
-    if not request.user.is_authenticated:
-        messages.error(request, 'Usuário não logado.')
-        return redirect('index')
+    #if not request.user.is_authenticated:
+        #messages.error(request, 'Usuário não logado.')
+        #return redirect('index')
 
     form  = PostForm()
     if request.method == 'POST':
@@ -82,5 +82,27 @@ def home(request):
 
 def logout(request):
     auth.logout(request)
-    messages.success('logout efetuado com sucesso.')
+    messages.success(request, 'logout efetuado com sucesso.')
     return redirect('index')
+
+def editar_post(request, post_id):
+    post = Post.objects.get(id=post_id)
+    form2 = PostForm(instance=post)
+    
+    if request.method == 'POST':
+        form2 = PostForm(request.POST, instance=post)
+
+        if form2.is_valid():
+            form2.save()
+            messages.success(request, 'post editado')
+            return redirect('home')
+        
+    return render(request,'home.html', {'form': form2, 'post_id': post_id}) 
+
+
+
+def deletar_post(request, post_id):
+    post = Post.objects.get(id=post_id)
+    post.delete()
+    messages.success(request, 'post deletado')
+    return redirect('home')
